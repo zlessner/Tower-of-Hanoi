8c32bcc3cfaffb8797de438e1faf0409edb48e29
Zachary Lessner <LessnerZachary@gmail.com>
2019-04-16 11:52:47 -0400

    HTML and CSS framework

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 console.log("Tower of Hanoi")
+
+//simulation through lowest amount of moves scenario in addition to user playing game
+
+//create three different arrays with winning array having correct order and pieces
